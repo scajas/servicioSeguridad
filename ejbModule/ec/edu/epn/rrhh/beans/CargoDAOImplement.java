f8441207ee5efc458cbe42d854b6bb8b7cbe62c8
Andres Cevallos <andrescevallosl@gmail.com>
2019-05-07 16:20:00 -0500

    Implementado metodo para la seleccion de cargos con estado migrado

--- ejbModule/ec/edu/epn/rrhh/beans/CargoDAOImplement.java
+++ ejbModule/ec/edu/epn/rrhh/beans/CargoDAOImplement.java
@@ -134,6 +134,14 @@ public class CargoDAOImplement extends DaoGenericoImplement<Cargosm> implements 
 		return resultado;
 	}
 	
+	@Override
+	public List<Cargosm> findCargosActivosMigrados() {
+		StringBuilder queryString = new StringBuilder(
+				"SELECT fam FROM Cargosm fam where fam.estado ='activo' or fam.estado = 'migrado' or fam.estado = 'inactivo'");
+		Query query = getEntityManager().createQuery(queryString.toString());
+		List<Cargosm> resultado = query.getResultList();
+		return resultado;	
+	}
 	
 	
 }
