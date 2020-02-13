3e5d13d35ae85af21b17c47ec3bd5a25b82a7b79
José Luis Escobar <jose.escobarc@epn.edu.ec>
2020-02-13 15:28:45 -0500

    Generado nuevo método para partida general.

--- ejbModule/ec/edu/epn/rrhh/beans/PartidaGeneralDAOImplement.java
+++ ejbModule/ec/edu/epn/rrhh/beans/PartidaGeneralDAOImplement.java
@@ -3,6 +3,7 @@ package ec.edu.epn.rrhh.beans;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
 
 import ec.edu.epn.generic.DAO.DaoGenericoImplement;
@@ -38,4 +39,25 @@ public class PartidaGeneralDAOImplement extends DaoGenericoImplement<PartidaGene
 		return (int) query.getSingleResult();
 	}
 
+	@Override
+	public boolean isPartidaAntigua(String numeroPartida) {
+		StringBuilder queryString =
+				new StringBuilder("Select pg from PartidaGeneral pg where "
+						+ "pg.numeroPartida = ?1 and estado = 'INACTIVO'");
+		Query query = getEntityManager().createQuery(queryString.toString());
+		query.setParameter(1, numeroPartida.substring(4));
+		try {
+			
+			PartidaGeneral partida = (PartidaGeneral) query.getSingleResult();
+			if(partida!=null) {
+				return true;
+			}else {
+				return false;
+			}
+			
+		}catch(NoResultException e)
+		{
+			return false;
+		}
+	}
 }
