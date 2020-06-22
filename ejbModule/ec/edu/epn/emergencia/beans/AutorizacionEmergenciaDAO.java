f6563e093a320395c5e18dfc41e8b92539c8e9e3
Andres Cevallos <andrescevallosl@gmail.com>
2020-06-22 15:02:17 -0500

    Cambios realizados por Requerimientos
    Se añaden campos nuevos a las entidades. se aniaden consultas pra buscar
    empleado pro Ingresar. Se aniade entidades y busqueda de autoridades

--- ejbModule/ec/edu/epn/emergencia/beans/AutorizacionEmergenciaDAO.java
+++ ejbModule/ec/edu/epn/emergencia/beans/AutorizacionEmergenciaDAO.java
@@ -2,29 +2,24 @@ package ec.edu.epn.emergencia.beans;
 
 import java.util.Date;
 import java.util.List;
-
-
 import javax.ejb.Remote;
-
 import ec.edu.epn.emergencia.entidades.AutorizacionEmergencia;
 import ec.edu.epn.generic.DAO.DaoGenerico;
 
-
-
 @Remote
-public interface AutorizacionEmergenciaDAO extends DaoGenerico<AutorizacionEmergencia> {
-
-	public List<AutorizacionEmergencia> findAutorizaciones();
-
-	public Integer getMaxId();
-
-	public List<AutorizacionEmergencia> findAutorizacionesXCedula(String cedula);
-
-	public List<AutorizacionEmergencia> findAutorizacionesXCedulaXFechaDesdexFechaHasta(String cedula, Date fechaDesde,
-			Date fechaHasta);
-
-	public List<AutorizacionEmergencia> findAutorizacionesXExpiradoAutorizado(Date fechaActual);
-
-	public List<AutorizacionEmergencia> findAutorizacionesXEstadoCalendarizado(Date fechaActual);
-
+public interface AutorizacionEmergenciaDAO extends DaoGenerico<AutorizacionEmergencia>
+{
+    List<AutorizacionEmergencia> findAutorizaciones();
+    
+    Integer getMaxId();
+    
+    List<AutorizacionEmergencia> findAutorizacionesXCedula(final String p0);
+    
+    List<AutorizacionEmergencia> findAutorizacionesXCedulaXFechaDesdexFechaHasta(final String p0, final Date p1, final Date p2);
+    
+    List<AutorizacionEmergencia> findAutorizacionesXExpiradoAutorizado(final Date p0);
+    
+    List<AutorizacionEmergencia> findAutorizacionesXEstadoCalendarizado(final Date p0);
+    
+    List<AutorizacionEmergencia> findAutorizacionesXEstadoPersona(final String p0);
 }
